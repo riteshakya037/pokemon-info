7698940cfa3e4b3de0b365d20fb77749f5b08d77
Ritesh Shakya <riteshakya037@gmail.com>
2020-08-09 18:52:03 +0545

    - Migrated main activity

--- app/src/main/java/com/riteshakya/pokemoninfo/ui/pokemon/screens/MainActivity.kt
+++ app/src/main/java/com/riteshakya/pokemoninfo/ui/pokemon/screens/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.riteshakya.pokemoninfo
+package com.riteshakya.pokemoninfo.ui.pokemon.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.riteshakya.pokemoninfo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
